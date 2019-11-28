f1904bac82e40f45c9234dcdd492de1718a0f402
2class-012 <2class-012@2class-012-PC>
2019-11-28 16:24:01 +0900

    123

---
+++ KDH/src/BoardUI/BoardDAO.java
@@ -0,0 +1,18 @@
+package BoardUI;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BoardDAO {
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	
+	
+	
+	
+	
+	
+}
